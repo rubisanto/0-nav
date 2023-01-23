db7de09fcfc77838c8576b5d3049164bb12a538e
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-23 16:58:14 +0100

    add page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/pages/contact/contact.component';
+import { AccueilComponent } from './home/accueil/accueil.component';
+import { PageNotFoundComponent } from './page-not-found/pages/page-not-found/page-not-found.component';
+import { ProductsListComponent } from './shop/pages/products-list/products-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AccueilComponent },
+  {path: 'contact', component: AccueilComponent},
+  {path: 'shop', component: ProductsListComponent},
+  // page not found
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
